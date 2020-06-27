d34403eca911b50fd3000b4819f1a1747c02bb7e
leanlimac <leandrolima7472@gmail.com>
2020-06-27 03:34:33 -0300

    HTTP methods, routes and resources annotations

--- src/Server.ts
+++ src/Server.ts
@@ -5,6 +5,18 @@ const app = express();
 app.get('/users', (request, response) =>{
     console.log('Listagem de usuários');
 
+    // Rota: Endereço completo da requisição
+    // Recurso: Qual entidade estamos acessando do sistema
+
+    //  GET: Buscar uma ou mais informações no back-end
+    //  POST: Criar uma nova informação no back-end
+    //  PUT: Atualizar uma informação existente no back-end
+    //  DELETE: Remover uma informação do back-end
+
+    //  POST http://localhost:3333/users = criar um usuário
+    //  GET http://localhost:3333/users = Listar usuários
+    //  GET http://localhost:3333/users/5 = Buscar dados do usuário com ID 5
+
     response.json(
         [
             'Diego',
@@ -13,6 +25,14 @@ app.get('/users', (request, response) =>{
             'Daniel'
         ]
     );
+})
+
+app.post('/users', (request, response) =>{
+    const user = {
+        name: 'Diego',
+        email: 'diego@rocketseat.com.br'
+    }
+    return response.json(user);
 })
 
 app.listen(3333);
