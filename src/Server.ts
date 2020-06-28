6a29acad275ade0c1cd4fb5fc90cea0ec80e30e5
leanlimac <leandrolima7472@gmail.com>
2020-06-28 09:46:59 -0300

    Param Types

--- src/Server.ts
+++ src/Server.ts
@@ -2,10 +2,16 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) =>{
-    console.log('Listagem de usuários');
+app.use(express.json());
 
-    // Rota: Endereço completo da requisição
+const users = [
+    'Diego',
+    'Cleiton',
+    'Robinson',
+    'Daniel'
+];
+
+// Rota: Endereço completo da requisição
     // Recurso: Qual entidade estamos acessando do sistema
 
     //  GET: Buscar uma ou mais informações no back-end
@@ -17,20 +23,33 @@ app.get('/users', (request, response) =>{
     //  GET http://localhost:3333/users = Listar usuários
     //  GET http://localhost:3333/users/5 = Buscar dados do usuário com ID 5
 
-    response.json(
-        [
-            'Diego',
-            'Cleiton',
-            'Robinson',
-            'Daniel'
-        ]
-    );
+    // TIPOS DE PARAMETROS
+
+    // Request Param: Parâmetros que vem na própria rota que identificam um recurso
+    // Query Param: Parâmetros que vem na própria rota geralmente opionais para filtros e paginações
+    // Request Body: Parâmetros para criação/atualização  de informações
+
+app.get('/users', (request, response) =>{
+    const search = String(request.query.search);
+
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+    return response.json(filteredUsers);
+})
+
+app.get('/users/:id', (request, response) =>{
+    const id = Number(request.params.id);
+    const user = users[id];
+
+    return response.json(user);
 })
 
 app.post('/users', (request, response) =>{
+    const data = request.body;
+
     const user = {
-        name: 'Diego',
-        email: 'diego@rocketseat.com.br'
+        name: data.name,
+        email: data.email
     }
     return response.json(user);
 })
