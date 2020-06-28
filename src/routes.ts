7a2d3738e7ae7fb729a38b15da1c63a49925c8c3
leanlimac <leandrolima7472@gmail.com>
2020-06-28 13:43:00 -0300

    Route Post points updating pivot and check when doesn't fail with transaction

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { request } from 'express';
 import knex from './database/connection';
 
 const routes = express.Router();
@@ -6,14 +6,54 @@ const routes = express.Router();
 routes.get('/items', async (request, response) =>{
     const items = await knex('items').select('*');
     
-    const serializedItems = items.map(items => {
+    const serializedItems = items.map(item => {
         return {
-            title: items.title,
-            image_url: `http://localhost:3333/uploads/${items.image}`, 
+            id: item.id,
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`, 
         };
     });
  
     return response.json(serializedItems);
+})
+
+routes.post('/points', async (request, response) =>{
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    }= request.body;
+
+    const trx = await knex.transaction();
+
+    const insertedIds = await trx('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    })
+
+    const point_id = insertedIds[0];
+
+    const pointItems = items.map((item_id : number) => {
+        return {
+            item_id,
+            point_id,
+        }
+    })        
+    
+    await trx('points_items').insert(pointItems);
+    
+    return response.json({success: true})
 })
 
 export default routes;
